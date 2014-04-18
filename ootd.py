02a3fe8b134d7ed75c316c57d0282bcd80ca0eab
DoctorPain99 <dr.pain99@gmail.com>
2014-04-17 22:02:17 -0700

    OutOfTouchDinosaur Version 0.2
    Added join, part, quit, and nick commands. Minimal user rights support.
    Cleaned up the code a bit (but not fully). Fixed an issue where it
    wouldn't automatically join channels on certain networks.

--- ootd.py
+++ ootd.py
@@ -1,11 +1,20 @@
 #importing necessary libraries
 import socket
 import random
+import fnmatch
+
+#some default strings
+defaultnick = ""
+ownerhost = ""
+fantasyletter = ""
 
 #prompt the user for some necessary variables
 server = raw_input("Enter the server you would like your bot to connect to: ")
 channel = raw_input("Enter the channel(s) you would like your bot to connect to: ")
-botnick = raw_input("Enter the nick you would like your bot to connect as: ")
+botnick = raw_input("Enter . to use the default bot nick, or enter the nick you would like your bot to connect as: ")
+
+if botnick == ".":
+   botnick = defaultnick
 
 #some basic functions
 def ping(msg):
@@ -14,46 +23,105 @@ def ping(msg):
    for i in msg:
       if servchar == True:
          pingServer += i
-      elif i == ":":
+      elif i == " ":
          servchar = True
    irc.send("PONG " + pingServer + "\n")
    print "PONG " + pingServer
 
 def sendmsg(chan, msg):
    irc.send("PRIVMSG " + chan + " :" + msg + "\n")
+   print "PRIVMSG " + chan + " :" + msg
 
 def changenick(nick):
-   botnick = nick
-   irc.send("NICK " + botnick + "\n")
+   irc.send("NICK " + nick + "\n")
+   print "NICK " + nick 
 
 def joinchan(chan):
    irc.send("JOIN " + chan + "\n")
+   print "JOIN " + chan
+
+def partchan(chan):
+   irc.send("PART " + chan + " :You all have no tech skill because you are all Nintendo.\n")
+   print "PART " + chan + " :You all have no tech skill because you are all Nintendo."
 
 def hello(chan):
    sendmsg(chan, "Hello. :|O")
 
+def get(begin,end,msg):
+   returnString = ""
+   char = False
+   for i in msg:
+      if char == True:
+         if i == end:
+            break
+         else:
+            returnString += i
+      elif i == begin:
+         char = True
+   return returnString 
+
+def getArg(num,msg):
+   returnString = ""
+   foundColon = False
+   foundFantasy = False
+   char = False
+   numSpaces = 0
+   for i in msg:
+      if char == True:
+         if i == " ":
+            break
+         else:
+            returnString += i
+      elif foundFantasy == True:
+         if i == " ":
+            numSpaces += 1
+         if numSpaces == num:
+            char = True
+      elif foundColon == True:
+         if i == fantasyletter:
+            foundFantasy = True
+         else:
+            foundColon = False
+      elif i == ":":
+         foundColon = True
+   return returnString
+
+def getNewNick(msg):
+   returnString = ""
+   char = False
+   numColons = 0
+   for i in msg:
+      if char == True:
+         returnString += i
+      elif i == ":":
+         numColons += 1
+         if numColons == 2:
+            char == True
+      return returnString
+               
+
 def getChannel(msg):
-        chanchar = False
-        channel = ""					
-	for i in msg:
-           if i == "#":
-              chanchar = True
-              channel += "#"
-           elif chanchar == True:
-             if i == " ":
-               break
-             else:
-              channel += i
-	return channel
+   return "#" + get("#"," ",msg)
 
 def getNick(msg):
-        nick = ""					
-	for i in msg:
-           if i == "!":
-              break
-           elif i != ":":
-              nick += i
-	return nick
+   return get(":","!",msg)
+
+def getHost(msg):
+   return get(":"," ",msg) 
+
+def stayConnected():
+
+   #print messages to console
+   if (ircmsg != ""):
+      print(ircmsg)
+
+   #terminate on disconnects
+   else:
+      disconnect = True
+
+   #reply to pings from the server
+   if ircmsg.find("PING :") != -1:
+      ping(ircmsg)
 
 
 #define some random responses
@@ -63,6 +131,7 @@ wellResponses = [
 ]
 
 #connect the bot
+disconnect = False
 irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 irc.connect((server, 6667))
 
@@ -70,26 +139,54 @@ irc.connect((server, 6667))
 irc.send("USER " + botnick + " " + " " + botnick + " " + botnick + " :This bot doesn't do shit.\n")
 changenick(botnick)
 
-#join channel(s)
-joinchan(channel)   
+#stay connected and join channels when ready
+while disconnect == False:
 
-#receive data from the server
-while (True):
    ircmsg = irc.recv(2048)
    ircmsg = ircmsg.strip('\n\r')
 
-   #print messages to console
-   if (ircmsg != ""):
-      print(ircmsg)
+   stayConnected()
 
-   #terminate on disconnects
-   else:
-      break
+   #join once connected and break to the next loop
+   if ircmsg.find(botnick) != -1:
+      joinchan(channel)
+      break 
+  
+#run commands as necessary
+while disconnect == False:
 
-   #reply to pings from the server
-   if ircmsg.find("PING :") != -1:
-      ping(ircmsg)
-   
+   ircmsg = irc.recv(2048)
+   ircmsg = ircmsg.strip('\n\r')
+
+   stayConnected()
+
+   #check to see if nick has changed and act accordingly
+   if (ircmsg.find("NICK :") != -1) and (ircmsg.find(":" + botnick) != -1):
+      botnick = getNewNick(ircmsg)
+
+   #owner only commands
+   if fnmatch.fnmatch(getHost(ircmsg).lower(),ownerhost):
+
+      #@die
+      if (ircmsg.find(":" + fantasyletter + "die") != -1):
+         irc.send("QUIT :" + fantasyletter + "ded\n")
+         print "QUIT :" +fantasyletter + "ded"
+         break
+
+      #@join and @part
+      if (ircmsg.find(":" + fantasyletter + "join") != -1):
+         joinchan(getArg(1,ircmsg))
+
+      if (ircmsg.find(":" + fantasyletter + "part") != -1):
+         if getArg(1,ircmsg) == "":
+            partchan(getChannel(ircmsg))
+         else:
+            partchan(getArg(1,ircmsg))
+
+      #@nick
+      if (ircmsg.find(":" + fantasyletter + "nick") != -1):
+         changenick(getArg(1,ircmsg))
+
    #say hello!
    if ircmsg.lower().find(":hello " + botnick.lower()) != -1:
       hello(getChannel(ircmsg))
